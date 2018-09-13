520f3f7ecf0b8a9d7383325c32da49761c9eb8ad
Abdul Samad <abdulsamad19951@gmail.com>
2018-09-14 00:44:59 +0500

    Changed greeting

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/',(req,res) =>{
-    res.send({hi:'hello there'});
+    res.send({hi:'hello there bye'});
 });
 
 const PORT = process.env.PORT || 5000;
